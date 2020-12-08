0f6b2c832b61f5cbc38a91b346c3216694e6b838
Incon <75150765+crosssitescriptin@users.noreply.github.com>
2020-12-08 17:59:51 -0600

    Update pe.py

--- ROPgadget/ropgadget/loaders/pe.py
+++ ROPgadget/ropgadget/loaders/pe.py
@@ -1,9 +1,9 @@
 ## -*- coding: utf-8 -*-
 ##
-##  Jonathan Salwan - 2014-05-12 - ROPgadget tool
+##  incon - 2014-05-12 - ROPgadget tool
 ##
-##  http://twitter.com/JonathanSalwan
-##  http://shell-storm.org/project/ROPgadget/
+##  http://twitter.com/Hexdumping
+##  
 ##
 
 from capstone   import *
