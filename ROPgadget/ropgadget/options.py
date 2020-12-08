b3f2f2c952626a717a9fff1edc7fe8ca9dd0bf2f
Incon <75150765+crosssitescriptin@users.noreply.github.com>
2020-12-08 17:52:50 -0600

    Update options.py

--- ROPgadget/ropgadget/options.py
+++ ROPgadget/ropgadget/options.py
@@ -1,11 +1,10 @@
 ## -*- coding: utf-8 -*-
 ##
-##  Jonathan Salwan - 2014-05-17 - ROPgadget tool
+##  incon - 2014-05-12 - ROPgadget tool
 ##
-##  http://twitter.com/JonathanSalwan
-##  http://shell-storm.org/project/ROPgadget/
+##  http://twitter.com/Hexdumping
+##  
 ##
-
 import re
 import codecs
 from capstone   import *
